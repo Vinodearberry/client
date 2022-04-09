96f26c8bc0afa2ada18d4b45f0ed6b1cbb68ed57
Vino <vinothfrost@gmail.com>
2022-04-09 18:47:38 +0530

    added styed components

---
+++ src/pages/Landing.js
@@ -0,0 +1,30 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/Landing";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            An app that lets users post jobs and also find jobs, the app has
+            search functionality and filtering options along with pagination
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
